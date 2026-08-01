68973771cbbfcbe43fccd1f4a4dc0022a6780e48
Nick <babukhnikita@gmail.com>
2026-08-01 11:53:42 +0200

    add: __init__ moved prediction function to app folder

---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+from . import data, features, models, serving, utils
+
+__all__ = ["data", "features", "models", "serving", "utils"]
---
+++ src/data/__init__.py
@@ -0,0 +1,4 @@
+from .data_loader import load_data
+from .preprocess import preprocess_data
+
+__all__ = ["load_data", "preprocess_data"]
