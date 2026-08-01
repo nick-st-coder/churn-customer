08616951df147e0ffa3d15445eaf28608d7af60e
Nick <babukhnikita@gmail.com>
2026-08-01 12:46:10 +0200

    modified: serving deleted

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,3 @@
-from . import data, features, models, serving, utils
+from . import data, features, models, utils
 
-__all__ = ["data", "features", "models", "serving", "utils"]
+__all__ = ["data", "features", "models", "utils"]
